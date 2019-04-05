ea79948ec7ef4f4bc1ccba0f573f79faa959cf74
RitaVeikuts <ritaveikuts@gmail.com>
2019-04-05 21:31:48 +0300

    well

--- src/by/belhard/j2/lesson2/Main.java
+++ src/by/belhard/j2/lesson2/Main.java
@@ -1,4 +1,45 @@
 package by.belhard.j2.lesson2;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        /*int x=666;
+
+        /*if (x==20) {
+            x--;
+        } else if (x==21){
+            x++;
+        } else {
+            x/=2;
+        }*/
+
+       // int y = (x % 2 == 0) ? (x/2):(-x);
+
+
+
+        /*System.out.println((x%2==0)?(x/2):(-x));*/
+
+/*int i=6;
+while (i>0){
+    System.out.println(i);
+    i--;
+}
+System.out.println("START");*/
+
+/*int i=0;
+do  {
+    System.out.println(i);
+    i++;
+} while (i!=3);
+        System.out.println("START");*/
+
+
+/*for (int i=-2; i<5; i+=2){
+    System.out.println(i);}*/
+
+
+    }
+
+
 }
---
+++ src/by/belhard/j2/lesson2/gjh.java
@@ -0,0 +1,13 @@
+package by.belhard.j2.lesson2;
+
+
+public class gjh {
+    public static void main(String[] args) {
+
+    for (int i=-2; i<5; i+=2) {
+        if (i==2)
+            break;
+        System.out.println(i);
+    }
+    System.out.println("END");
+}}
